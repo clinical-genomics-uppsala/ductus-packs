2a53146b68c86b1b7e118d8ab848b755d86f4de6
Patrik Smeds <patrik.smeds@gmail.com>
2024-03-26 12:57:05 +0100

    chore: remove old action/workflow/script

--- actions/lib/archive_wp2_truesight_myeloid_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-RUNFOLDER_FOLDER_PATH=${1}
-
-echo " ./archive/Bioinformatik/scripts/Copy_WP2_within_LUL.sh -e ${RUNFOLDER_FOLDER_PATH}"
-
-./archive/Bioinformatik/scripts/Copy_WP2_within_LUL.sh -e ${RUNFOLDER_FOLDER_PATH}
-
